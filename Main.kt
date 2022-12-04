d179a001e71730fd1e4e5bed1d99ab2fb83bad2b
nbietry <nicolas.bierty@loccitane.com>
2022-12-04 23:20:16 +0100

    Day2 - step1 - result

--- Main.kt
+++ Main.kt
@@ -1,13 +1,28 @@
-import java.io.BufferedReader
 import java.io.File
 
 fun main() {
-    val bufferedReader: BufferedReader = File("Input.txt").bufferedReader()
-    val inputString = bufferedReader.use { it.readText() }
+    val input = File("Input.txt").readText()
 
-    val input = inputString.split("\n\n")
+    val valuesDict = mapOf("A" to 1, "B" to 2, "C" to 3)
+    val correspond = mapOf("X" to "A", "Y" to "B", "Z" to "C")
+    val rules = mapOf("A A" to 3, "B B" to 3, "C C" to 3, "A B" to 0, "A C" to 6, "B A" to 6, "B C" to 0, "C A" to 0,
+        "C B" to 6)
+    val valuePlayer = input.split('\n').map { x-> x.split(' ')[0] + " " + correspond[x.split(' ')[1]]}
+    println(valuePlayer)
+    val scorePlayer1 = valuePlayer.map { x -> rules[x]!!.toInt() + valuesDict[x.split(' ')[0]]!!.toInt() }.sum()
+    val scorePlayer2 = valuePlayer.map { x -> rules[x.reversed()]!!.toInt() + valuesDict[x.split(' ')[1]]!!.toInt() }
+        .sum()
+    println(scorePlayer1.toString() + " " + scorePlayer2.toString())
+
+    /*
     val arrayOfElf = input.map { x-> x.split('\n').toList().map{it.toInt()}.reduce() { x, y -> x + y  } }
         .sortedDescending()
     println(arrayOfElf[0] + arrayOfElf[1] + arrayOfElf[2])
+     */
 }
 
+fun transpose( matrix: List<List<String>> ) {
+    return matrix.first().forEachIndexed {
+            i, _ -> matrix.map { it.getOrNull(i) }
+    }
+}
