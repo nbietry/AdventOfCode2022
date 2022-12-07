6e263dc043e6019300363dba433c69ba9c766145
nbietry <nicolas.bierty@loccitane.com>
2022-12-07 10:25:19 +0100

    Day5 - step1 - result clean

--- Main.kt
+++ Main.kt
@@ -1,16 +1,28 @@
 import java.io.File
+import java.util.*
 
 fun main() {
-    val input = File("Input.txt").readLines()
-    val compare = input.map { x -> compareSequence(x.split(',')[0], x.split(',')[1]) }.count { x -> x }
-    println(compare)
-}
+    val input = File("Input.txt").readText().split("\n\n")
+    val part1 = input[0].lines().reversed().map {  x -> x.chunked(4)}
+    val part2 = input[1].lines()
 
-fun compareSequence(seq1: String, seq2: String): Boolean{
-    val firstNumber = (seq1.split('-')[0].toInt() <= seq2.split('-')[0].toInt())
-    val secondNumber = (seq1.split('-')[1].toInt() >= seq2.split('-')[0].toInt())
-    val firstNumberReverse = (seq2.split('-')[0].toInt() <= seq1.split('-')[0].toInt())
-    val secondNumberReverse = (seq2.split('-')[1].toInt() >= seq1.split('-')[0].toInt())
-    val result = (firstNumber && secondNumber) || (firstNumberReverse && secondNumberReverse)
-    return  (firstNumber && secondNumber) || (firstNumberReverse && secondNumberReverse)
+    val stack : List<ArrayDeque<String>> = List(part1.size) {ArrayDeque<String>()}
+    for((indexLin, line) in part1.withIndex()){
+        for((indexCol, column) in line.withIndex()){
+            if(!column.isNullOrBlank()) stack[indexCol].push(column)
+        }
+    }
+
+    for(move in part2){
+        val match = Regex("(\\w+) (\\d+) (\\w+) (\\d+) (\\w+) (\\d+)").find(move)!!
+        val (move, qty, from, src, to, dst) = match.destructured
+        for(i in 1..qty.toInt())
+            stack[dst.toInt()-1].addFirst(stack[src.toInt()-1].pop())
+    }
+
+    var response: String = ""
+    for(column in stack)
+        response += column.pop()
+
+    println(response.replace("[", "").replace("]", "").replace(" ", ""))
 }
